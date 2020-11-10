264cc90877068501e97813d1fbcdea2cc267accc
Tamizhvendan S <tamizh88@gmail.com>
2020-11-10 16:22:48 +0530

    add get users api

--- hs-src/Foo/Main.hs
+++ hs-src/Foo/Main.hs
@@ -1,5 +1,38 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Foo.Main where
-import HaskellSay(haskellSay)
+
+import Data.Aeson
+import GHC.Generics
+import Servant
+import qualified Network.Wai.Handler.Warp as Warp
+
+data User = User
+  { name :: String,
+    age :: Int,
+    email :: String
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+type UserAPI = "users" :> Get '[JSON] [User]
+users =
+  [ User "John Doe" 28 "johndoe@example.com",
+    User "Ram Sundar" 32 "ram@example.com"
+  ]
+
+server :: Server UserAPI
+server = return users
+
+userProxy :: Proxy UserAPI
+userProxy = Proxy
+
+
+app = serve userProxy server
+
 main :: IO ()
 main = do
-    haskellSay "hello!"
+  Warp.run 8081 app
